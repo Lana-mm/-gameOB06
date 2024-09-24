43340080d345d0efa5ada86bb1a7326db48c9b0a
Lana-mm <malimonovas@list.ru>
2024-09-24 22:10:18 +0500

    class Game

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ class Hero:
 
     def is_alive(self):
         return self.health > 0
+class Game:
+    def __init__(self, player_name):
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
+
+    def start(self):
+        print("Игра началась!")
+        while self.player.is_alive() and self.computer.is_alive():
+            self.player.attack(self.computer)
+            if not self.computer.is_alive():
+                print(f"{self.computer.name} повержен! {self.player.name} — победитель!")
+                break
+
+            self.computer.attack(self.player)
+            if not self.player.is_alive():
+                print(f"{self.player.name} повержен! {self.computer.name} — победитель!")
+                break
